666cda33a73682b171a2d227b89e72a3f688a89d
Anushkapawar2005 <ap2244034@gmail.com>
2025-12-01 11:46:53 +0530

    Started python learning

---
+++ Chapter1/Practice Set 1/problem4.py
@@ -0,0 +1,12 @@
+import os
+
+# directory
+directory_path = '/'
+
+# list all files and directories in the specified path
+contents = os.listdir(directory_path)
+
+#print each file & directory
+print(contents)
+# for item in contents:
+#   print(item)
---
+++ Chapter1/module.py
@@ -0,0 +1,9 @@
+import pyjokes
+# print("printing jokes")    // short cut ctl + / or use type #
+"""
+so thanks 
+that was my program
+ 
+"""
+joke = pyjokes.get_joke()
+print(joke)
