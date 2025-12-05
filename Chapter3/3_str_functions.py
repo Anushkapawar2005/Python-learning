5f38d49e503d8da85e5f5052bf2d15f586352fab
Anushkapawar2005 <ap2244034@gmail.com>
2025-12-05 23:24:17 +0530

    add more functions

--- Chapter3/3_str_functions.py
+++ Chapter3/3_str_functions.py
@@ -1,5 +1,23 @@
-name = "Anushka"
+name = "anushka"
 
-print(len(name))
-print(name.endswith("shka"))
-print(name.startswith("Anu"))
+print(len(name))   #7
+print(name.endswith("shka"))  # true
+print(name.startswith("Anu"))   # False
+print(name.capitalize())   # Anushka 
+print(name.title())     #Anushka
+print(name.count("a"))   # 2
+print(name.find('n'))    # 1
+print(name.index('u'))   # 2
+print(name.replace('a','T'))  #TnushkT
+print(name.upper())  # ANUSHKA
+print(name.lower())  # anushka
+print(name.casefold())  # anushka
+print(name.isascii())   # true
+print(name.isalnum())   # true
+print(name.center(10, '#'))
+
+
+str = "  hi My name is  anu   "
+print(str.strip())
+print(str.lstrip())
+print(str.rstrip())
