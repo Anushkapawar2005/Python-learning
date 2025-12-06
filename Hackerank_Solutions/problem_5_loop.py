afff148e094be94abdc35cd1c63a785c58d61f55
Anushkapawar2005 <ap2244034@gmail.com>
2025-12-06 19:14:10 +0530

    add solution

---
+++ Hackerank_Solutions/problem_5_loop.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    n = int(input())
+    
+    # for i in range(n):
+    #     print(i**2)
+        
+    i = 0
+    while(i<n):
+        print(i*i)
+        i+=1
+    
