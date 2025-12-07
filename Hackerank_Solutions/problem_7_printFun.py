ebdfc5dbcc607f9ecc30e9494bf1d95e1dd4aa4e
Anushkapawar2005 <ap2244034@gmail.com>
2025-12-07 14:45:58 +0530

    add solution

---
+++ Hackerank_Solutions/problem_7_printFun.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    n = int(input())
+    
+    for i in range(1,n+1):
+        print(i,end="")  #normally print() moves to newline after printing. Using end="" does not print a newline.
