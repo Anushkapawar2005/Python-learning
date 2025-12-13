bfa9d933daeb1b0ca77c81b89f3b50df0f3c7abc
Anushkapawar2005 <ap2244034@gmail.com>
2025-12-13 23:08:33 +0530

    Delete Chapter10 directory

--- Chapter10/2_instance_vs_class_attri.py
+++
@@ -1,8 +0,0 @@
-class Employee:
-  language = "py"   # this is a class attribute
-  salary = 12000
-
-anu = Employee()
-anu.language = "JavaScript"  # this is an object (instance) attribute
-print( anu.language,anu.salary)
-
