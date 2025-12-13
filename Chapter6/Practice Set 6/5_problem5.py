5f1dbb9c16eed6652febb244e4b07564e4787427
Anushkapawar2005 <ap2244034@gmail.com>
2025-12-13 23:10:18 +0530

    Delete Chapter6 directory

--- Chapter6/Practice Set 6/5_problem5.py
+++
@@ -1,7 +0,0 @@
-l = ["Ram", "Shyam", "Seeta", "Geeta"]
-name = input("Enter your name: ")
-
-if(name in l):
-  print("Your name is in the list")
-else:
-  print("Your name is not in the list")
--- Chapter6/Practice Set 6/7_problem.7.py
+++
@@ -1,6 +0,0 @@
-post = "Hey aNu is good anu is very good and anu is great"
-
-if("Anu".lower() in post.lower()):
-  print("This post is  talking about anu")
-else:
-  print("This post is not talking about anu")
