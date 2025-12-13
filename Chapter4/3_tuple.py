32588fec3bd7247ed78800da586d55e8c64a36bc
Anushkapawar2005 <ap2244034@gmail.com>
2025-12-13 23:09:50 +0530

    Delete Chapter4 directory

--- Chapter4/3_tuple.py
+++
@@ -1,5 +0,0 @@
-# tuple are immutable
-a = (1,2,5,6,False,"Anu")  
-b = (1,) # only one element
-print(a)
-print(type(a))
--- Chapter4/Practice set 4/3_problem3.py
+++
@@ -1,2 +0,0 @@
-a=(34,124,"Anu")
-a[2]="Hii"    # it give error becoz tuples are immutable
