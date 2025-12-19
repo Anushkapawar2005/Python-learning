0cfce2ac4546decf47688ed7bc5d273f145917de
Anushkapawar2005 <ap2244034@gmail.com>
2025-12-19 22:39:16 +0530

    add more ex

--- 04_list_tuples/3_tuple.py
+++ 04_list_tuples/3_tuple.py
@@ -1,5 +1,48 @@
-# tuple are immutable
+# tuple are immutable  - so tuples are not modify - 
+# tuple faster than list - python don't need to manage changes to them like list elements are fixed
+
+n = () # empty tuples
 a = (1,2,5,6,False,"Anu")  
 b = (1,) # only one element
-print(a)
-print(type(a))
+c = (1, [1,5,6], (4,5,6))
+d = 1, 2, 3, 4
+
+Name, age = "Anushka", 75
+
+print(a)  # (1, 2, 5, 6, False, 'Anu')
+print(c)  # (1, [1, 5, 6], (4, 5, 6))
+
+
+# accessing tuple items
+print(c[2])     # (4, 5, 6)
+print(a[-2])
+print(type(a))  # <class 'tuple'>
+
+# modify a tuple - raise error
+#a[1] = 56  # error
+# but, you can create new tuple with updated value
+new_num = a + (76,)
+print(new_num)  # (1, 2, 5, 6, False, 'Anu', 76)
+
+
+# packingn - PUTTING MULTIPLE VALUes into a single tuple
+name = "anu"
+age = 20
+pack_tuple = name,age 
+print(pack_tuple)  # ('anu', 20)
+
+# unpacking - extracting the value from a tuple 
+person  = ("anu", 20)
+name , age = person  
+print(name)     # anu
+print(age)      # 20
+
+
+# type casting or add value in tuple
+my_tuple = ("apple","mango","cherry")
+# type cast tuple to list
+y = list(my_tuple)
+y.append("orange")
+# type cast back list to tuple
+my_tuple = tuple(y)
+print(my_tuple)    # ('apple', 'mango', 'cherry', 'orange')
--- 04_list_tuples/4_tuple_methods.py
+++ 04_list_tuples/4_tuple_methods.py
@@ -1,8 +1,27 @@
 
 a = (1,2,5,6,False,"Anu")  
-no = a.count(6)
-print(no)
+print(len(a))  # 6
+print(a.count(6)) # 1
+print(a.index(5)) # 2
 
-i = a.index(5)
-print(i)
-print(len(a))
+T1 = (2,6,7,5)
+T2 = (4,3,1,3)
+print(sum(T1))  # 20
+print(max(T1))  # 7
+print(min(T1))  # 2
+
+sorted_NUM = sorted(T1)
+print(sorted_NUM)  # [2, 5, 6, 7]
+
+
+combined = T1 + T2
+print(combined)
+
+t3 = ("hello",) * 3
+print(t3)     # ('hello', 'hello', 'hello')
+
+# in - checking for an item 
+print(3 in T1)  # False
+
+# slicing
+print(a[1:4])   # (2, 5, 6)
