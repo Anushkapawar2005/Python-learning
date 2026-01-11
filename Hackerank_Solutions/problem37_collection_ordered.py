4d1f2b524fd4aebc03f8609e60bf2e3302e4f807
Anushkapawar2005 <ap2244034@gmail.com>
2026-01-11 22:49:28 +0530

    add solution

---
+++ Hackerank_Solutions/problem37_collection_ordered.py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from collections import OrderedDict
+od = OrderedDict()
+for i in range(int(input())):
+    item_name,blank,price = input().rpartition(' ')
+    if item_name not in od:
+        od[item_name] = int(price)
+    else:
+        od[item_name] += int(price)
+        
+for i in od.items():
+    print(*i)
