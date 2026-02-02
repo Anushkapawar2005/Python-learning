c3270209b21334eadade97867ae6bb0641f21fb3
Anushkapawar2005 <ap2244034@gmail.com>
2026-02-02 11:10:52 +0530

    add solution

---
+++ Hackerank_Solutions/problem59_set_symm_diff.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+eng = int(input())
+eng_roll = set(map(int,input().split()))
+french = int(input())
+french_roll= set(map(int,input().split()))
+
+print(len(eng_roll.symmetric_difference(french_roll)))
